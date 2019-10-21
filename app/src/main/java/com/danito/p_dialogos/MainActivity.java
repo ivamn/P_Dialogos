a866bc5dcc7e4bc6375235d33990af0f69244ebf
Iván Gallego Torres <id.gallego16@iesdoctorbalmis.com>
2019-10-21 17:50:43 +0200

    Añadido archivos del proyecto

---
+++ app/src/main/java/com/danito/p_dialogos/MainActivity.java
@@ -0,0 +1,46 @@
+package com.danito.p_dialogos;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button bIniciar = findViewById(R.id.botonIniciar);
+        bIniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                LayoutInflater inflater = MainActivity.this.getLayoutInflater();
+                View dialogView = inflater.inflate(R.layout.alta_usuario, null);
+                builder.setView(dialogView);
+                Button bCrear = dialogView.findViewById(R.id.botonCrear);
+                bCrear.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(getApplicationContext(), "Se creará la cuenta", Toast.LENGTH_LONG);
+                    }
+                });
+
+                Button bEntrar = dialogView.findViewById(R.id.botonEntrar);
+                bEntrar.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(getApplicationContext(), "Se iniciará la aplicación", Toast.LENGTH_LONG);
+                    }
+                });
+                builder.show();
+            }
+        });
+    }
+}
